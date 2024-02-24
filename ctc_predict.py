0e074bd93e79cfeeee745575c7fcd167f9a60758
FreshMag <pesco.magnani@gmail.com>
2024-02-24 17:21:32 +0100

    Fixed flags inside ctc_predict.py and deleted unnecessary files

--- ctc_predict.py
+++ ctc_predict.py
@@ -4,8 +4,10 @@ import ctc_utils
 import cv2
 import numpy as np
 
+tf.compat.v1.disable_eager_execution()
+
 parser = argparse.ArgumentParser(description='Decode a music score image with a trained model (CTC).')
-parser.add_argument('-image',  dest='image', type=str, required=True, help='Path to the input image.')
+parser.add_argument('-image', dest='image', type=str, required=True, help='Path to the input image.')
 parser.add_argument('-model', dest='model', type=str, required=True, help='Path to the trained model.')
 parser.add_argument('-vocabulary', dest='voc_file', type=str, required=True, help='Path to the vocabulary file.')
 args = parser.parse_args()
@@ -14,7 +16,7 @@ tf.compat.v1.reset_default_graph()
 sess = tf.compat.v1.InteractiveSession()
 
 # Read the dictionary
-dict_file = open(args.voc_file,'r')
+dict_file = open(args.voc_file, 'r')
 dict_list = dict_file.read().splitlines()
 int2word = dict()
 for word in dict_list:
@@ -24,7 +26,7 @@ dict_file.close()
 
 # Restore weights
 saver = tf.compat.v1.train.import_meta_graph(args.model)
-saver.restore(sess,args.model[:-5])
+saver.restore(sess, args.model[:-5])
 
 graph = tf.compat.v1.get_default_graph()
 
@@ -40,12 +42,12 @@ WIDTH_REDUCTION, HEIGHT = sess.run([width_reduction_tensor, height_tensor])
 
 decoded, _ = tf.nn.ctc_greedy_decoder(logits, seq_len)
 
-image = cv2.imread(args.image,False)
+image = cv2.imread(args.image, cv2.IMREAD_GRAYSCALE)
 image = ctc_utils.resize(image, HEIGHT)
 image = ctc_utils.normalize(image)
-image = np.asarray(image).reshape(1,image.shape[0],image.shape[1],1)
+image = np.asarray(image).reshape(1, image.shape[0], image.shape[1], 1)
 
-seq_lengths = [ image.shape[2] / WIDTH_REDUCTION ]
+seq_lengths = [image.shape[2] / WIDTH_REDUCTION]
 
 prediction = sess.run(decoded,
                       feed_dict={
@@ -55,6 +57,18 @@ prediction = sess.run(decoded,
                       })
 
 str_predictions = ctc_utils.sparse_tensor_to_strs(prediction)
+
+
+def write_strings_to_file(strings, filename):
+    with open(filename, 'w') as file:
+        for string in strings:
+            file.write(string + '\t')  # Separating strings by "\t"
+        file.write('\n')  # Adding a newline at the end
+
+
+output = []
+
 for w in str_predictions[0]:
-    print (int2word[w]),
-    print ('\t'),
+    output.append(int2word[w])
+
+write_strings_to_file(output, "output.semantic")
